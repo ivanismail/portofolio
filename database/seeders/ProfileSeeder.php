0a72b37f8843362498e9fb5d71a04fa8c297598b
ivanismail <ivanismail2@gmail.com>
2022-05-09 11:54:35 +0700

    add: profile seeder data

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProfileSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class ProfileSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // insert data ke table profile
+        DB::table('profiles')->insert([
+        	'name' => 'Ivan Ismail',
+        	'phone_number' => '+62 8211-303-2503',
+        	'email' => 'ivanismail2@gmail.com',
+        	'city' => 'Bekasi',
+            'province' => 'Jawa Barat',
+            'photo' => '',
+            'twitter' => 'https://twitter.com/ivanismail2',
+            'facebook' => 'https://www.facebook.com/ivanismailrespect',
+            'linked' => 'https://www.linkedin.com/in/ivan-ismail-163840200/',
+            'instagram' => 'https://www.instagram.com/ivanismail2/',
+            'start_experience' => 2017
+        ]);
     }
 }
