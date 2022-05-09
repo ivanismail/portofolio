0a08a05d53661c63bf229e7a71d36a2651a0948d
ivanismail <ivanismail2@gmail.com>
2022-05-09 10:10:00 +0700

    add: set model profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'phone_number', 'email', 'city', 'province', 'photo' ,'twitter', 'facebook', 'linked', 'instagram', 'start_experience'
+    ];
+
+    public function GetCreatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+    
+    public function GetUpdatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
 }
