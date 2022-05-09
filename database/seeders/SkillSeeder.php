4bbe7dac96b3238fd89ac2d337b7ca12a2946053
ivanismail <ivanismail2@gmail.com>
2022-05-09 11:42:10 +0700

    add: skill seeder dummy

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class SkillSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 4; $i++){
+ 
+    	      // insert data ke table skill menggunakan Faker
+    		DB::table('skills')->insert([
+    			'skill_name' => $faker->jobTitle,
+    			'percent' => $faker->numberBetween(60,85),
+    		]);
+ 
+    	}
     }
 }
